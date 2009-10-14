3b373265aaf5d84fb1371332e1b7271878319a27
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2009-10-13 20:47:02 -0500

    Create answers model and show all answers for each question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,9 @@
 class Question < ActiveRecord::Base
+  has_many :answers
+
   validates_presence_of :title, :body
+
+  def answer_count
+    "#{answers.count} answers"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091013003522) do
+ActiveRecord::Schema.define(:version => 20091014011711) do
+
+  create_table "answers", :force => true do |t|
+    t.text     "body"
+    t.integer  "question_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "questions", :force => true do |t|
     t.string   "title"
