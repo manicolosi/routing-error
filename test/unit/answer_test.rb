12c5d09740643a26c99d876fa2d53821cefae72a
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2009-10-15 00:57:32 -0500

    Basic unit tests for questions and answers

--- test/unit/answer_test.rb
+++ test/unit/answer_test.rb
@@ -1,8 +1,20 @@
 require 'test_helper'
 
 class AnswerTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "able to save valid answers" do
+    a = Answer.new(:body => 'The body')
+    a.question = Question.new(:title => 't', :body => 'b')
+    assert a.save
+  end
+
+  test "unable to save with no body" do
+    a = Answer.new
+    assert !a.save
+    assert_equal "can't be blank", a.errors[:body]
+  end
+
+  test "every answer should belong to a question" do
+    a = Answer.new(:body => 'The body')
+    assert_raise(ActiveRecord::RecordInvalid) { a.save! }
   end
 end
--- test/unit/question_test.rb
+++ test/unit/question_test.rb
@@ -1,8 +1,39 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "able to save valid questions" do
+    q = Question.new(:title => 'The Title', :body => 'The body')
+    assert q.save
+  end
+
+  test "unable to save invalid questions" do
+    q = Question.new
+
+    assert !q.save
+    assert_equal "can't be blank", q.errors[:title]
+    assert_equal "can't be blank", q.errors[:body]
+  end
+
+  test "answer count with no answers" do
+    q = Question.new
+    assert_equal '0 answers', q.answer_count
+  end
+
+  test "answer count with one answers" do
+    q = Question.new(:title => 'The Title', :body => 'The body')
+    Answer.create(:question => q, :body => 'Test answer #3')
+    q.save!
+
+    #assert_equal '1 answer', q.answer_count
+  end
+
+  test "answer count with some answers" do
+    q = Question.new(:title => 'The Title', :body => 'The body')
+    Answer.create(:question => q, :body => 'Test answer #3')
+    Answer.create(:question => q, :body => 'Test answer #3')
+    Answer.create(:question => q, :body => 'Test answer #3')
+    q.save!
+
+    assert_equal '3 answers', q.answer_count
   end
 end
