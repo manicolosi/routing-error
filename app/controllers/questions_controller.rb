1bb9d97fea2c506c684541d43f159c1414afd684
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2009-10-17 23:19:28 -0500

    Allow asking and editing questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,4 +6,34 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(params[:question])
+
+    if @question.save
+      flash[:notice] = 'Successfully asked your question.'
+      redirect_to @question
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+
+    if @question.update_attributes(params[:question])
+      flash[:notice] = "Updated question successfully."
+      redirect_to @question
+    else
+      render :action => 'edit'
+    end
+  end
 end
