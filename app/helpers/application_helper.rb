f3a6d5f2a391f0ba070b123238bfc565563bb457
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2009-10-18 23:23:36 -0500

    Helper method to display flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def flash_messages
+    content_tag(:div, :id => 'flash_messages') do
+      flash.collect do |key,msg|
+        content_tag(:div, msg, :class => key)
+      end
+    end
+  end
 end
