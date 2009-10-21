ce1e917c31d4230bb92fd51761a475129ead2b77
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2009-10-21 14:43:09 -0500

    Fix some methods that ended up in the wrong place

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,22 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
+  helper_method :current_user_session, :current_user, :logged_in?
+
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-  # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  private
+
+  def current_user_session
+    return @current_user_session if defined? @current_user_session
+    @current_user_session = UserSession.find
+  end
+
+  def current_user
+    @current_user = current_user_session && current_user_session.user
+  end
+
+  def logged_in?
+    current_user != nil
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,25 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  private
-
   def flash_messages
     content_tag(:div, :id => 'flash_messages') do
       flash.collect do |key,msg|
         content_tag(:div, msg, :class => key)
       end
     end
-  end
-
-  def current_user_session
-    return @current_user_session if defined? @current_user_session
-    @current_user_session = UserSession.find
-  end
-
-  def current_user
-    @current_user = current_user_session && current_user_session.user
-  end
-
-  def logged_in?
-    current_user != nil
   end
 end
