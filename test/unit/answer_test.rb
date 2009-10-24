ba3fc581061cf0914a002176a43430b3e0a3ef50
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2009-10-23 19:59:46 -0500

    Updated unit tests to reflect author requirement

--- test/unit/answer_test.rb
+++ test/unit/answer_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class AnswerTest < ActiveSupport::TestCase
   test "able to save valid answers" do
-    a = Answer.new(:body => 'The body')
+    u = User.new(:username => 'joe', :email => 'joe@example.com')
+    a = Answer.new(:author => u, :body => 'The body')
     a.question = Question.new(:title => 't', :body => 'b')
     assert a.save
   end
@@ -11,6 +12,7 @@ class AnswerTest < ActiveSupport::TestCase
     a = Answer.new
     assert !a.save
     assert_equal "can't be blank", a.errors[:body]
+    assert_equal "can't be blank", a.errors[:author]
   end
 
   test "every answer should belong to a question" do
--- test/unit/question_test.rb
+++ test/unit/question_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
   test "able to save valid questions" do
-    q = Question.new(:title => 'The Title', :body => 'The body')
+    u = User.new(:username => 'joe', :email => 'joe@example.com')
+    q = Question.new(:author => u, :title => 'The Title', :body => 'The body')
     assert q.save
   end
 
@@ -12,5 +13,6 @@ class QuestionTest < ActiveSupport::TestCase
     assert !q.save
     assert_equal "can't be blank", q.errors[:title]
     assert_equal "can't be blank", q.errors[:body]
+    assert_equal "can't be blank", q.errors[:author]
   end
 end
