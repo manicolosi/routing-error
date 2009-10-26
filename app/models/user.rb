2e844a86d5341eee8cd785d976873f5ae65a3e54
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2009-10-25 21:37:26 -0500

    Show up and down vote counts on profile pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
 class User < ActiveRecord::Base
   has_many :questions, :foreign_key => 'author_id'
   has_many :answers, :foreign_key => 'author_id'
+  has_many :votes, :foreign_key => 'voter_id'
 
   acts_as_authentic
 
   def to_param
     username
+  end
+
+  def vote_ups
+    votes.all(:conditions => "value = '1'").count
+  end
+
+  def vote_downs
+    votes.all(:conditions => "value = '-1'").count
   end
 end
