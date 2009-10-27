511db0ea3c748084e6f615b2662d292b461c86e1
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2009-10-27 15:32:56 -0500

    Cleaned up recent activity rendering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @recent = @user.recent_activity
   end
 
   def new
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,22 @@
 module UsersHelper
-  def render_activity_for(activity)
-    subdir = activity.class.to_s.downcase.pluralize # Yuck!
-    partial = "#{subdir}/profile_activity"
-    render :partial => partial, :locals => { :activity => activity }
+  def render_recent_activities(user)
+    render :partial => 'recent_activity',
+      :locals => { :recent_activity => user.recent_activity }
+  end
+
+  def activity_title(activity)
+    send "#{activity.class.to_s.underscore}_title", activity
+  end
+
+  def activity_time(activity)
+    "#{time_ago_in_words(activity.created_at)} ago"
+  end
+
+  def question_title(activity)
+    "Asked &ldquo;#{activity.title}&rdquo;"
+  end
+
+  def answer_title(activity)
+    "Answered &ldquo;#{activity.question.title}&rdquo;"
   end
 end
