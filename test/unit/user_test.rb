1125e33d729becfeb65f5df92531ec0e16b11fdc
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2009-10-28 19:15:18 -0500

    Unit tests for user reputation

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,51 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "a new user has zero reputation ponts" do
+    user = users(:example)
+    assert_equal 0, user.reputation
+  end
+
+  test "an upvote is worth 10 reputation points" do
+    user = users(:question_author)
+    question = questions(:example)
+    question.author = user
+    question.save
+
+    voter = users(:example)
+    voter.votes.create!(:voteable => question, :value => 1)
+
+    assert_equal 10, user.reputation
+  end
+
+  test "an downvote is worth -2 reputation points" do
+    user = users(:question_author)
+    question = questions(:example)
+    question.author = user
+    question.save
+
+    voter = users(:example)
+    voter.votes.create!(:voteable => question, :value => -1)
+
+    assert_equal -2, user.reputation
+  end
+
+  test "2 upvotes and 2 downvotes is worth 16 reputation points" do
+    user = users(:question_author)
+    4.times do
+      question = questions(:example).clone
+      question.author = user
+      question.save
+    end
+
+    voter = users(:example)
+    user.questions[0, 2].each do |question|
+      voter.votes.create!(:voteable => question, :value => 1)
+    end
+    user.questions[2, 2].each do |question|
+      voter.votes.create!(:voteable => question, :value => -1)
+    end
+
+    assert_equal 16, user.reputation
   end
 end
